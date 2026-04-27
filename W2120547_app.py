241f1e11a5eeeea1c65e9614872988cd746201dc
Jalangana Samarasekara <jalanganasamarasekara@gmail.com>
2026-04-27 15:49:41 +0530

    page configuration

--- W2120547_app.py
+++ W2120547_app.py
@@ -2,3 +2,10 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+# PAGE CONFIG
+st.set_page_config(
+    page_title="Global Exchange Rate Dashboard",
+    page_icon="💱",
+    layout="wide"
+)
